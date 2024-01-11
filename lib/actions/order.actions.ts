a27f6a05db88fa9b94faa78fe04cad0fbadaa578
sndpw <sandeepkumartirumala@gmail.com>
2024-01-11 18:39:26 +0530

    updated

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -20,7 +20,7 @@ export const checkoutOrder = async (order: CheckoutOrderParams) => {
       line_items: [
         {
           price_data: {
-            currency: 'usd',
+            currency: 'ind',
             unit_amount: price,
             product_data: {
               name: order.eventTitle
