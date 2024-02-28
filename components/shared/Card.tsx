95016cbc6d7a4052476992aa80854e4f03fd25aa
Sandeep Kumar <125688774+sndpw@users.noreply.github.com>
2024-02-28 13:53:54 +0530

    Update Card.tsx

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -44,7 +44,7 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
           <span className="p-semibold-14 w-min rounded-full bg-green-100 px-4 py-1 text-green-60">
             {event.isFree ? 'FREE' : `Rs.${event.price}`}
           </span>
-          <p className="p-semibold-14 w-min rounded-full bg-grey-500/10 px-4 py-1 text-grey-500 line-clamp-1">
+          <p className="p-semibold-14 w-min rounded-full bg-grey-500/10 px-4 py-1 text-grey-500 line-clamp-2">
             {event.category.name}
           </p>
         </div>}
